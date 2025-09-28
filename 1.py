e0d0f95e388a9af3de26faed0a2a5269eda85969
avel0041 <vel-ar@mail.ru>
2025-09-28 12:16:24 +0300

    8_2

--- 1.py
+++
@@ -1,7 +0,0 @@
-alphabet = sorted(set('0123456789QWERTYUIOPASDFGHJKLZXCVBNM'))
-for p in range(8,36):
-    for x in alphabet[0:p]:
-        for y in alphabet[0:p]:
-            if int('45', p) * int('67', p)==int(x + y + '2', p):
-                print(int(y+x, p))
-                break
