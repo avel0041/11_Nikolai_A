4fdfd9bc1df54f3c51ed59dd75fc22d20e2d6b1e
avel0041 <vel-ar@mail.ru>
2025-10-14 21:08:13 +0300

    12

--- 1.py
+++ 1.py
@@ -1,2 +1,10 @@
+def f(s, e, v):
+    if s>e:
+        return 0
+    if s==e:
+        return 1
+    if ('11' in v) or ('22' in v):
+        return 0
+    return f(s+1, e, v+'1') + f(s+2, e, v+'1') + f(s*2,e,v+'2') + f(s*3, e,v+'2') 
 
-
+print(f(1, 24, ''))
