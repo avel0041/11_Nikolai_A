3a98144e0ccd89a2e248105b9b3d0cbff2eae130
avel0041 <vel-ar@mail.ru>
2025-10-19 12:33:13 +0300

    13

--- 1.py
+++ 1.py
@@ -1,10 +1,14 @@
-def f(s, e, v):
-    if s>e:
-        return 0
-    if s==e:
-        return 1
-    if ('11' in v) or ('22' in v):
-        return 0
-    return f(s+1, e, v+'1') + f(s+2, e, v+'1') + f(s*2,e,v+'2') + f(s*3, e,v+'2') 
-
-print(f(1, 24, ''))
+f={}
+g={}
+for n in range(0,130,-1):
+    if n<3:
+        g[n], f[n] = 1, 1
+    if n>2 and n%2==0:
+        f[n] = g[n]+f[n-1]
+    if n > 2 and n%2!=0:
+        f[n] = f[n-2] - g[n + 1]
+    if n>2 and n%2==0:
+        g[n] = f[n-3]+f[n-2]
+    if n > 2 and n%2!=0:
+        g[n] = f[n+1] -g[n-1]
+print(g[120])
