d3f0f2657dcaa59acd245c1d0d475af684c777ca
avel0041 <vel-ar@mail.ru>
2025-11-11 19:51:11 +0300

    15

--- 1.py
+++ 1.py
@@ -1,24 +1,57 @@
-k=0
-res=[]
-res1=[]
-f = open('17.txt').readlines()
-for s in f:
-    if int(s)>99 and int(s)<1000 and int(s)%10==8:
-        z1=int(s)**2
-        res.append(z1)
-z=min(res)
-a = list(map(int,f))
-for i in range(len(f)-2):
-    if (99<a[i]<1000 or 99<a[i+1]<1000 or 99<a[i+2]<1000):
-        u1 = 0
-        u2 = 0
-        u3 = 0
-        if a[i]**2>z:
-            u1 = 1
-        if a[i+1]**2>z:
-            u2 = 1
-        if a[i+2]**2>z:
-            u3 = 1
-        if u1+u2+u3==2:
-            res1.append(a[i]+a[i+1]+a[i+2])
-print(len(res1),max(res1))
+def delitel(n):
+    res=[]
+    for x in range(1,int(n**0.5)+1):
+        if n%x==0:
+            res.append(x)
+            res.append(n//x)
+    return set(res)
+
+def delitel1(n):
+    res=[]
+    x=1
+    while x**2<=n:
+        if n%x==0:
+            res.append(x)
+            res.append(n//x)
+        x+=1
+    return set(res)
+    
+def prost_del(n):
+    res=[]
+    x = 2
+    while x*x<=n:
+        while n%x==0:
+            res.append(x)
+            n = n//x
+        x+=1
+    if n > 1:
+        res.append(int(n))
+    return res
+
+
+# x = 600_000
+# q = []
+# a = []
+# c = 0
+# while c < 5:
+#     for i in range(17, int(x**0.5) + 1, 10):
+#         if x%i==0:
+#             q.append(x)
+#             a.append(i)
+#             c+=1
+#             break
+#     x += 1
+# print(q, a)
+
+res = []
+for n in range(2_000_000,3_000_001):
+    c=0
+    for i in range(1000,int(n**0.5)+1):
+        if n%i==0:
+            if n//i-i<=115:
+                c+=1
+                if c>=3:
+                    res.append(int(n))
+                    break
+print(res)
+
