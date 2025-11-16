e66ad2fb970806d8e8fb6c9f4f3ab1eeba7c15d4
avel0041 <vel-ar@mail.ru>
2025-11-16 12:10:21 +0300

    16

--- 1.py
+++ 1.py
@@ -1,57 +1,23 @@
-def delitel(n):
+def f(x):
     res=[]
-    for x in range(1,int(n**0.5)+1):
-        if n%x==0:
-            res.append(x)
-            res.append(n//x)
-    return set(res)
-
-def delitel1(n):
-    res=[]
-    x=1
-    while x**2<=n:
-        if n%x==0:
-            res.append(x)
-            res.append(n//x)
-        x+=1
-    return set(res)
-    
-def prost_del(n):
-    res=[]
-    x = 2
-    while x*x<=n:
-        while n%x==0:
-            res.append(x)
-            n = n//x
-        x+=1
-    if n > 1:
-        res.append(int(n))
-    return res
-
-
-# x = 600_000
-# q = []
-# a = []
-# c = 0
-# while c < 5:
-#     for i in range(17, int(x**0.5) + 1, 10):
-#         if x%i==0:
-#             q.append(x)
-#             a.append(i)
-#             c+=1
-#             break
-#     x += 1
-# print(q, a)
-
-res = []
-for n in range(2_000_000,3_000_001):
-    c=0
-    for i in range(1000,int(n**0.5)+1):
-        if n%i==0:
-            if n//i-i<=115:
-                c+=1
-                if c>=3:
-                    res.append(int(n))
-                    break
-print(res)
+    n=2
+    while n*n<=x:
+        if x%n==0:
+            x//=n
+            res.append(n)
+        else:
+            n+=1
+    if x>1:
+        res.append(int(x))
+    if len(res)==1:
+        return 0
+    return max(res)-min(res)
+k=0
+for i in range(3300000, 100000000000000000000000):
+    if f(i)%10==5:
+        if str(f(i))==str(f(i))[::-1]:
+            print(i, f(i))
+            k+=1
+            if k==5:
+                break
 
